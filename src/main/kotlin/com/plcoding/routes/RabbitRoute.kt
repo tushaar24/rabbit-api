c6944f94c20083a7a8c8f43f71bf6158a6609c77
Tushar <paltushar150@gmail.com>
2021-11-29 01:05:35 +0530

    initial commit

---
+++ src/main/kotlin/com/plcoding/routes/RabbitRoute.kt
@@ -0,0 +1,25 @@
+package com.plcoding.routes
+
+import com.plcoding.data.model.Rabbit
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+private const val BASE_URL = "192.168.1.198:8080"
+private val rabbits = listOf(
+    Rabbit("Carl", "A cute brown rabbit", "$BASE_URL/rabbits/rabbit1.jpg"),
+    Rabbit("Emma", "Emma likes to eat apples", "$BASE_URL/rabbits/rabbit2.jpg"),
+    Rabbit("John", "John is very active", "$BASE_URL/rabbits/rabbit3.jpg"),
+    Rabbit("Neeti", "She is rude", "$BASE_URL/rabbits/rabbit4.jpg"),
+)
+
+fun Route.randomRabbit(){
+    get("/randomrabbit"){
+        call.respond(
+            HttpStatusCode.OK,
+            rabbits.random()
+        )
+    }
+
+}
