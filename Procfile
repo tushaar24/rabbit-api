e02355159fae7b38cfd776c28cb55732af56c5aa
Tushar <paltushar150@gmail.com>
2021-12-01 03:16:46 +0530

    production ready

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/com.plcoding.ktor-rabbits/bin/com.plcoding.ktor-rabbits
--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
+tasks.create("stage") {
+    dependsOn("installDist")
+}
+
 repositories {
     mavenCentral()
 }
